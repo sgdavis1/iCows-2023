8aab96524deb52de5da9336d0776c43ed01cea85
Luke Pearson <l.pearson.vzw@gmail.com>
2022-02-10 10:30:41 -0600

    Bug fix for SwimController.php and update seed.sql

--- src/default/modules/swim/src/Controller/SwimController.php
+++ src/default/modules/swim/src/Controller/SwimController.php
@@ -18,22 +18,22 @@ class SwimController extends ControllerBase {
 
   public function show($id) {
 
-  # Use dynamic querys instead: https://www.drupal.org/docs/7/api/database-api/dynamic-queries/introduction-to-dynamic-queries
+  # Use dynamic queries instead: https://www.drupal.org/docs/7/api/database-api/dynamic-queries/introduction-to-dynamic-queries
   $query = \Drupal::database()->select('icows_swims', 'i');
   
   // Add extra detail to this query object: a condition, fields and a range
   $query->condition('i.swim_id', $id, '=');
-  // $query->fields('i', ['uid', 'swim_id', 'field_date', 'title', 'description', 'locked']);
-  // $query->range(0, 50);
+   $query->fields('i', ['uid', 'swim_id', 'field_date', 'title', 'description', 'locked']);
+   $query->range(0, 50);
   $result = $query->execute()->fetchAll()[0];
   return [
     '#theme' => 'show',
     '#id' => $id,
-    '#title' => $record->title,
-    '#description' => $record->description,
-    '#locked' => $record->locked,
-    '#date_time' => $record->field_date,
-    '#uid' => $record->uid,
+    '#title' => $result->title,
+    '#description' => $result->description,
+    '#locked' => $result->locked,
+    '#date_time' => $result->field_date,
+    '#uid' => $result->uid,
   ];    
   }
 
