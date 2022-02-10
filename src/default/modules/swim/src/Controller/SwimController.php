4a89a5ca7c3160b422abd0e3113feb01848f79f9
kian-weimer <55513580+kian-weimer@users.noreply.github.com>
2022-02-10 10:05:16 -0600

    Added swim controller

--- src/default/modules/swim/src/Controller/SwimController.php
+++ src/default/modules/swim/src/Controller/SwimController.php
@@ -18,13 +18,23 @@ class SwimController extends ControllerBase {
 
   public function show($id) {
 
-  $query = \Drupal::database()->query( "SELECT * FROM swims where swimID = $id" );
-  $results = $query->fetchAll();
-    return [
-      '#theme' => 'show',
-      '#id' => $id,
-      '#description' => $results[0]->description,
-    ];
+  # Use dynamic querys instead: https://www.drupal.org/docs/7/api/database-api/dynamic-queries/introduction-to-dynamic-queries
+  $query = \Drupal::database()->select('icows_swims', 'i');
+  
+  // Add extra detail to this query object: a condition, fields and a range
+  $query->condition('i.swim_id', $id, '=');
+  // $query->fields('i', ['uid', 'swim_id', 'field_date', 'title', 'description', 'locked']);
+  // $query->range(0, 50);
+  $result = $query->execute()->fetchAll()[0];
+  return [
+    '#theme' => 'show',
+    '#id' => $id,
+    '#title' => $record->title,
+    '#description' => $record->description,
+    '#locked' => $record->locked,
+    '#date_time' => $record->field_date,
+    '#uid' => $record->uid,
+  ];    
   }
 
   public function edit($id) {
