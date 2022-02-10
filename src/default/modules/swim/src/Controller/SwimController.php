9e4d11543e08101564e54612ff74e5917ab60623
kian-weimer <55513580+kian-weimer@users.noreply.github.com>
2022-02-10 11:20:05 -0600

    formatted swim date time string on the show page.

--- src/default/modules/swim/src/Controller/SwimController.php
+++ src/default/modules/swim/src/Controller/SwimController.php
@@ -7,6 +7,8 @@
 namespace Drupal\swim\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Datetime\DateHelper;
  
 class SwimController extends ControllerBase {
   public function content() {
@@ -23,16 +25,18 @@ class SwimController extends ControllerBase {
   
   // Add extra detail to this query object: a condition, fields and a range
   $query->condition('i.swim_id', $id, '=');
-   $query->fields('i', ['uid', 'swim_id', 'field_date', 'title', 'description', 'locked']);
-   $query->range(0, 50);
+  $query->fields('i', ['uid', 'swim_id', 'field_date', 'title', 'description', 'locked']);
+  $query->range(0, 50);
   $result = $query->execute()->fetchAll()[0];
+  $date = new DrupalDateTime($result->field_date, 'UTC');
+  
   return [
     '#theme' => 'show',
     '#id' => $id,
     '#title' => $result->title,
     '#description' => $result->description,
     '#locked' => $result->locked,
-    '#date_time' => $result->field_date,
+    '#date_time' => getFormattedDate($date),
     '#uid' => $result->uid,
   ];    
   }
@@ -50,4 +54,39 @@ class SwimController extends ControllerBase {
       '#id' => $id,
     ];
   }
+}
+
+function getFormattedDate($date) {
+  $day_index = DateHelper::dayOfWeek($date);
+  $day = "";
+  switch ($day_index) {
+    case 1:
+      $day = "Monday";
+      break;
+    case 2:
+      $day = "Tuesday";
+      break;
+    case 3:
+      $day = "Wednesday";
+      break;
+    case 4:
+      $day = "Thursday";
+      break;
+    case 5:
+      $day = "Friday";
+      break;
+    case 6:
+      $day = "Saturday";
+      break;
+    case 7:
+      $day = "Sunday";
+      break;
+    default:
+      $day = "Unknown";
+      break;
+  }
+  
+  $month_index = intval($date->format('m'));
+  $month = strval(DateHelper::monthNames()[$month_index]);
+  return $day . ", " . $month . $date->format(' d, Y - g:ia');
 }
