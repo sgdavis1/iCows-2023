2755a5171b56bc8aa99799bdaf52adac4300465e
kian-weimer <55513580+kian-weimer@users.noreply.github.com>
2022-02-10 10:05:38 -0600

    added new swim form

--- src/default/modules/swim/src/Form/NewSwimForm.php
+++ src/default/modules/swim/src/Form/NewSwimForm.php
@@ -4,6 +4,9 @@ namespace Drupal\swim\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\datetime\Plugin\Field\FieldType;
+
 
 /**
  * Implements an example form.
@@ -50,8 +53,6 @@ class NewSwimForm extends FormBase {
             '#required' => TRUE
         ];
 
-        //need to add sidebar option-selector thing
-
         $form['actions']['#type'] = 'actions';
         $form['actions']['submit'] = [
             '#type' => 'submit',
@@ -65,16 +66,38 @@ class NewSwimForm extends FormBase {
      * {@inheritdoc}
      */
     public function validateForm(array &$form, FormStateInterface $form_state) {
-        if (strlen($form_state->getValue('phone_number')) < 3) {
-            $form_state->setErrorByName('phone_number', $this->t('The phone number is too short. Please enter a full phone number.'));
-        }
+        // TODO: add validation
     }
 
     /**
      * {@inheritdoc}
      */
     public function submitForm(array &$form, FormStateInterface $form_state) {
-        $this->messenger()->addStatus($this->t('Your phone number is @number', ['@number' => $form_state->getValue('phone_number')]));
+        $locked = 0;
+        if ($form_state->getValue('override') == 'Locked') {
+            $locked = 1;
+        }
+
+        $values = [
+            [
+            'title' => $form_state->getValue('title'),
+            'description' => $form_state->getValue('description')["value"],// only saving value for now, can save format later
+            'locked' => $locked,
+            // https://drupal.stackexchange.com/questions/204103/inserting-the-value-from-datetime-field-form
+            // would need to add timezones here if we ever wanted to support multiple timezones
+            'field_date' => $form_state->getValue('date_time')->format(\Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface::DATETIME_STORAGE_FORMAT),
+            // must add uid, ask steve if uid determined by creator or from dropdowm
+            'uid' => \Drupal::currentUser()->id(),
+            ],
+        ];
+        $database = \Drupal::database();
+        $query = $database->insert('icows_swims')->fields(['title','description','locked','field_date']); //->values($values)->execute();
+        foreach ($values as $developer) {
+            $query->values($developer);
+        }
+        $query->execute();
+
+
     }
 
 }
