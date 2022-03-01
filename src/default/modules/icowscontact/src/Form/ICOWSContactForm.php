60d123f838fe54644915fcec83f6a0768c0c792c
rhuehnergarth <56234481+rhuehnergarth@users.noreply.github.com>
2022-03-01 02:10:16 -0600

    update form to have correct form items

--- src/default/modules/icowscontact/src/Form/ICOWSContactForm.php
+++ src/default/modules/icowscontact/src/Form/ICOWSContactForm.php
@@ -4,15 +4,15 @@ namespace Drupal\icowscontact\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Datetime\DrupalDateTime;
-use Drupal\datetime\Plugin\Field\FieldType;
-use Drupal\Core\Url;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Implements an example form.
  */
 class ICOWSContactForm extends FormBase {
+
+    public function getFormId() {
+        return 'icows_contact_form';
+    }
 
     /**
      * {@inheritdoc}
@@ -20,7 +20,7 @@ class ICOWSContactForm extends FormBase {
     public function buildForm(array $form, FormStateInterface $form_state) {
         $form['subject'] = [
             '#type' => 'textfield',
-            '#title' => $this->t('Pace'),
+            '#title' => $this->t('Subject'),
             '#maxlength' => 20,
             '#required' => TRUE
         ];
@@ -30,10 +30,9 @@ class ICOWSContactForm extends FormBase {
             '#maxlength' => 300,
             '#required' => TRUE
         ];
-//        $form['actions']['#type'] = 'actions';
         $form['actions']['submit'] = [
             '#type' => 'submit',
-            '#value' => $this->t('Sign up'),
+            '#value' => $this->t('Send Emails'),
             '#button_type' => 'primary',
         ];
         return $form;
@@ -52,4 +51,4 @@ class ICOWSContactForm extends FormBase {
 
     }
 
-} 
+}
